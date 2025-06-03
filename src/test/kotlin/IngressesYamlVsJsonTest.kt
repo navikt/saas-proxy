aa8b6cf5c7f8c8e6e9b66049e9d4583535add87f
Björn Hägglund <bjorn.hagglund@nav.no>
2025-06-03 12:20:49 +0200

    dokument1-q.adeo.no

--- src/test/kotlin/IngressesYamlVsJsonTest.kt
+++ src/test/kotlin/IngressesYamlVsJsonTest.kt
@@ -54,7 +54,8 @@ class IngressesYamlVsJsonTest {
                 .filterNot {
                     it.contains("dokument-test-354.adeo.no") ||
                         it.contains("otds-042.adeo.no") ||
-                        it.contains("empower1-q.adeo.no")
+                        it.contains("empower1-q.adeo.no") ||
+                        it.contains("dokument1-q.adeo.no")
                 }
                 .toSet()
 
