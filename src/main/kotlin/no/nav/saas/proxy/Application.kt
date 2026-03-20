adb7a14470d1ac5096f491db3314b3557850a473
Björn Hägglund <bjorn.hagglund@nav.no>
2026-03-20 10:18:08 +0100

    Store latest req-res for all statuses

--- src/main/kotlin/no/nav/saas/proxy/Application.kt
+++ src/main/kotlin/no/nav/saas/proxy/Application.kt
@@ -246,11 +246,11 @@ object Application {
                         )}) - call time $totalCallTime ms ($handlingTokenTime handling, $redirectCallTime redirect)"
                     }
 
-                    if ((!response.status.successful && response.status.code != 404) || response.status.code == 201) {
-                        File(
-                            "/tmp/latest-$targetApp-${response.status.code}",
-                        ).writeText("${currentDateTime}\nREDIRECT:\n${redirect.toMessage()}\n\nRESPONSE:\n${response.toMessage()}")
-                    }
+                    // if ((!response.status.successful && response.status.code != 404) || response.status.code == 201) {
+                    File(
+                        "/tmp/latest-$targetApp-${response.status.code}",
+                    ).writeText("${currentDateTime}\nREDIRECT:\n${redirect.toMessage()}\n\nRESPONSE:\n${response.toMessage()}")
+                    // }
 
                     try {
                         val tokenType = "proxy:${if (TokenExchangeHandler.isOBOToken(token)) "obo" else "m2m"}"
